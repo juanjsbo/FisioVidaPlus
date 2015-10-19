d29c50c10f3ae1b38ac75f34d0eafa1b4700d817
carmego11 <carmego11@hotmail.com>
2015-10-19 01:50:11 -0500

    Styles y Botones de Action Bar

--- app/src/main/java/com/fisiovidaplus/juan/carlos/fisiovidaplus/MainActivity.java
+++ app/src/main/java/com/fisiovidaplus/juan/carlos/fisiovidaplus/MainActivity.java
@@ -1,17 +1,50 @@
 package com.fisiovidaplus.juan.carlos.fisiovidaplus;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+import static com.fisiovidaplus.juan.carlos.fisiovidaplus.R.*;
+import static com.fisiovidaplus.juan.carlos.fisiovidaplus.R.color.title_text;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        Toolbar toolbar=(Toolbar) findViewById(id.toolbar);
+        toolbar.setTitle(string.app_name);
+        toolbar.setTitleTextColor(getResources().getColor(title_text));
+        toolbar.setLogo(drawable.medical_heart);
+
+        if(toolbar!=null){
+            setSupportActionBar(toolbar);
+        }
+        /*if(android.os.Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            Window window=getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.primary_dark,null));
+
+        }*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.BLUE);
+        }
+
     }
 
     @Override
@@ -29,7 +62,13 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_info) {
+            Toast.makeText(MainActivity.this, "Desarrolladores:\n\tJuan Sebastián Burgos\n\tCarlos Andrés Medina", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (id == R.id.action_exit) {
+            System.exit(0);
             return true;
         }
 
